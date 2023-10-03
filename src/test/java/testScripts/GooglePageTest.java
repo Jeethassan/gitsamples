3e0161a289a3ce51e6d04baebde0aa1b7edb46a9
Administrator <Administrator@172.31.2.144>
2023-10-03 07:04:14 +0000

    Initial Project

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,10 @@
+package testScripts;
+
+public class GooglePageTest {
+
+	public static void main(String[] args) {
+WebDriver driver = new ChromeDriver();
+driver.get("https://www.google.com/");
+	}
+
+}
